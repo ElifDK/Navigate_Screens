f52def7238d9f2b57da844d419b4d2b0b9a9e2e6
ElifDK <elif.koc@sabanciuniv.edu>
2025-03-12 10:12:13 +0300

    Added Second Screen

--- lib/main.dart
+++ lib/main.dart
@@ -35,8 +35,48 @@ class FirstScreen extends StatelessWidget {
                 foregroundColor: Colors.white),
               onPressed: () {
                 // Navigate to Second Screen
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => SecondScreen()));
                 },
               child: const Text('Go to Second Screen'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// Second Screen
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Second Screen'),
+        backgroundColor: Colors.blue,
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'This is the Second Screen!',
+              style: TextStyle(fontSize: 20),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue, // background
+                  foregroundColor: Colors.white),
+              onPressed: () {
+                // Navigate back to First Screen
+                Navigator.pop(context);
+              },
+              child: const Text('Go Back'),
             ),
           ],
         ),
